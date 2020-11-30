0e94ae45266abc111537d5aa50bbde4ae16190e0
Sunbreak <sunbreak.wang@gmail.com>
2020-11-30 17:15:50 +0800

    Add iOS project
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

---
+++ ios/Classes/HybridCompositionPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface HybridCompositionPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/HybridCompositionPlugin.m
@@ -0,0 +1,20 @@
+#import "HybridCompositionPlugin.h"
+
+@implementation HybridCompositionPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"hybrid_composition"
+            binaryMessenger:[registrar messenger]];
+  HybridCompositionPlugin* instance = [[HybridCompositionPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
